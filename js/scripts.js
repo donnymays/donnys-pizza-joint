5165c4ea9c5659d3109bb96343ed0c06f43dfc60
donnymays <dtmays2@gmail.com>
2020-08-28 12:06:16 -0700

    there's a disconnect occuring in the UI, nothing is appending when the submit button is pushed

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,37 @@
 function PizzaOrder(size, toppings) {
   this.size = size;
-  this.toppings = toppings;
-  this.total = this.pizzaTotal();
+  this.toppings = [];
 }
 
-PizzaOrder.prototype.pizzaTotal = function() {
-  let defaultPizza = 10;
-  if (this.size === "Medium") {
-    defaultPizza += 2
-  } if (this.size === "Large") {
-    defaultPizza += 5
-  } if (this.size === "MEGAPIZZA") {
-    defaultPizza += 10
-  } if (this.toppings === "pepperoni") {
-    defaultPizza += 2
-  } if (this.toppings === "sausage") {
-    defaultPizza += 3
-  }if (this.toppings === "mushroom") {
-    defaultPizza += 4
+PizzaOrder.prototype.pizzaTotal = function(toppingArr) {
+  let toppingTotal = 0;
+  for (i=0; i < toppingArr.length; i++) {
+    toppingTotal += toppingArr[i];
   }
-  return defaultPizza;
- 
+  let pizzaTotal = toppingTotal + this.size
+  return pizzaTotal;
 };
+
 
 
 $(document).ready(function()  {
   $("pizzaForm").submit(function (event) {
     event.preventDefault();
     
+    
 
-
-    let inputtedSize = $("size").val();
-    let inputtedTopping = $("topping").val();
-    console.log(inputtedSize, inputtedTopping);
-    let newPizza = new PizzaOrder(inputtedSize, inputtedTopping);
-    console.log(newPizza);
+    let inputtedSize = parseInt($("input:radio[name=size]:checked").val());
+    
+    let pizzaOrdered = new PizzaOrder(inputtedSize, toppings);
+    
+    $("input:checkbox[name=toppings]:checked").each(function(){
+      pizzaOrdered.toppings.push(parseInt($(this).val()));
+    });
+    
+    let orderTotal = pizzaOrdered.pizzaTotal(pizzaOrdered.toppings);
+    
    
-
-    $("#totalPrice").text(defaultPizza);
+    $("#totalPrice").append(orderTotal)
 
  
   });
