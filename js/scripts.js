b92b8831993e5ce9b2e9c729023aa628c85a53fd
donnymays <dtmays2@gmail.com>
2020-08-28 08:13:04 -0700

    started method to calculate pizza price

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,15 @@ function PizzaOrder(toppings, size) {
   this.toppings = toppings;
   this.size = size;
 }
+
+PizzaOrder.prototype.orderTotal = funtion() {
+  let basePrice = 12;
+  if (this.size === "Medium") {
+    basePrice += 3;
+  } if (this.size === "Large") {
+    basePrice += 6;
+  } if (this.size === "MEGA PIZZA") {
+    basePrice += 10;
+  }
+  return basePrice;
+}
