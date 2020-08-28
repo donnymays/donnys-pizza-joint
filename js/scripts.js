6442c6d311139631b91c276081a0df128987d6bf
donnymays <dtmays2@gmail.com>
2020-08-28 09:46:15 -0700

    functions and methods for calcualting total cost

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 function PizzaOrder(toppings, size) {
-  this.toppings = toppings;
+  this.toppings = [];
   this.size = size;
 }
 
-PizzaOrder.prototype.orderTotal = function() {
+PizzaOrder.prototype.sizeTotal = function() {
   let basePrice = 12;
   if (this.size === "Medium") {
     basePrice += 3;
@@ -15,10 +15,29 @@ PizzaOrder.prototype.orderTotal = function() {
   return basePrice;
 }
 
+PizzaOrder.prototype.toppingsTotal = function(toppingsArr){
+  let toppingsPrice = 0;
+  for (i=0; i<toppingsArr.length; i++) {
+    toppingsPrice += toppingsArr[i];
+  }
+  return toppingsPrice
+}
+
+
+
 
 $(document).ready(function () {
-  $("").submit(function (event) {
+  $("pizzaForm").submit(function (event) {
     event.preventDefault();
  
+    function addToppings() {
+      $("input:checkbox[name=toppings]:checked").each(function(){
+        PizzaOrder.toppings.push(parseInt($(this).val()));
+      });
+
+    $("#totalPrice").append(pizzaTotal);
+
+
+
   });
 });
